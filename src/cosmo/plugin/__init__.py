fe0de984046c682f48c706909450c2b63bc1ed48
Dan Marchese <dan@marchese.me>
2025-08-01 21:26:32 -0700

    Enabling plugins to do custom routes

--- src/cosmo/plugin/__init__.py
+++ src/cosmo/plugin/__init__.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 from collections.abc import AsyncGenerator
 
+from fastapi.routing import APIRouter
+
 from cosmo.plugin.model import AbstractCondition
 
 
@@ -14,4 +16,8 @@ class CosmoPlugin(ABC):
 
     def get_rule_utility(self) -> object | None:
         """Returns the rule utility for this plugin.  Default is to not have a utility"""
+        return None
+
+    def configure_routes(self, router: APIRouter) -> None:
+        """Invoked to define any custom server routes for this plugin"""
         return None
